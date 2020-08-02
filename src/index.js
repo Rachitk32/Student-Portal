e1a8f54f9c23708954a3cc14ba2878b5f84c6920
thepulkitgupta <thepulkitgupta@gmail.com>
2020-08-03 01:05:09 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+function Fn()
+{
+  return <div class="container-fluid" style={{height:100+"vh"}}>
+  <div class="d-flex flex-column justify-content-center align-items-center" style={{height:100+"%"}}>
+    <div class="d-flex flex-column bg-white rounded p-5" id="element">
+      <form action="#" >
+        <div class="form-group">
+          <label for="email">Email</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <div class="input-group-text">@</div>
+            </div>
+            <input class="form-control" id="email" type="text" placeholder="Enter Email" name="email" size="30" required/>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="pwd">Password</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <div class="input-group-text">#</div>
+            </div>
+            <input class="form-control" id="pwd" type="password" placeholder="Enter Password" name="pwd" required/>
+          </div>
+        </div>
+
+        <div class="form-group form-check">
+          <input class="form-check-input" id="remme" type="checkbox"/>
+          <label class="form-check-label" for="remme">Remember Me</label>
+        </div>
+
+        <button class="btn btn-success">Submit</button>
+        
+      </form>
+    </div>
+  </div>
+  </div>
+};
+
+const element=<Fn/>
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
