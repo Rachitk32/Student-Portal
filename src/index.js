4284b3ea0a7465c23d11287edc2cdc61e6ee2e44
thepulkitgupta <thepulkitgupta@gmail.com>
2020-08-07 16:45:27 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Register from './Register';
-//import Clock from './Clock';
-import LoginForm from './LoginForm';
-//import Toggle from './Toggle';
+import Mainpage from './Mainpage';
 
 ReactDOM.render(
-  // uncomment any one to see the component
- //<Register/>
- //<Toggle/>
- //<Clock/>
- <LoginForm/>
-  ,document.getElementById('root')
+ <Mainpage/>,
+  document.getElementById('root')
 );
