6d4b073ffc8d817c6c9617d982011d178f8b1d98
thepulkitgupta <thepulkitgupta@gmail.com>
2020-08-07 16:50:14 +0530

    .

---
+++ build files/precache-manifest.2eede741a817e850745d0b643e56bb74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e73de6934ff043125c03aa9db3716b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9f11c97e11f8b68537",
+    "url": "/static/js/2.1c914c60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c914c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18459f11a6c6abc0bce",
+    "url": "/static/js/main.6be7543e.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  },
+  {
+    "revision": "da594bbb81f4ce3f39365173cf9e4607",
+    "url": "/static/media/Eye.da594bbb.png"
+  }
+]);
