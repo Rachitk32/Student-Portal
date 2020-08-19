33fdef0f761f61fd743d8244db1b54df620a3896
Rachitk32 <65167268+Rachitk32@users.noreply.github.com>
2020-08-19 20:18:39 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Mainpage from './Mainpage';
-import Homepage from './Homepage';
-
-ReactDOM.render(
- <Homepage/>,
-  document.getElementById('root')
-);
